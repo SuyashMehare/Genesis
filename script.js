fcc95c7a4211b8e599fb092503925bb5d75a3980
Rupayan Das <hrupayandas@gmail.com>
2025-08-19 01:48:30 +0530

    added ball collision and bounce functionality

--- script.js
+++ script.js
@@ -7,14 +7,13 @@ let playerWidth = 80;
 let playerHeight = 10;
 let playerVelocityX = 10;
 
-
 let player = {
-    x: boardWidth/2 - playerWidth/2,
-    y: boardHeight - playerHeight - 5,
-    width: playerWidth,
-    height: playerHeight,
-    velocityX : playerVelocityX
-}
+  x: boardWidth / 2 - playerWidth / 2,
+  y: boardHeight - playerHeight - 5,
+  width: playerWidth,
+  height: playerHeight,
+  velocityX: playerVelocityX,
+};
 
 let ballWidth = 10;
 let ballHeight = 10;
@@ -22,58 +21,93 @@ let ballVelocityX = 3;
 let ballVelocityY = 2;
 
 let ball = {
-    x: boardWidth/2,
-    y: boardHeight/2,
-    width : ballWidth,
-    height : ballHeight,
-    velocityX : ballVelocityX,
-    velocityY : ballVelocityY
-}
+  x: boardWidth / 2,
+  y: boardHeight / 2,
+  width: ballWidth,
+  height: ballHeight,
+  velocityX: ballVelocityX,
+  velocityY: ballVelocityY,
+};
 
 window.onload = function () {
-    board = document.getElementById("board");
-    board.height = boardHeight;
-    board.width = boardWidth;
-    context = board.getContext("2d");
+  board = document.getElementById("board");
+  board.height = boardHeight;
+  board.width = boardWidth;
+  context = board.getContext("2d");
 
-    context.fillStyle = "lightgreen";
-    context.fillRect(player.x, player.y, player.width, player.height);
+  context.fillStyle = "lightgreen";
+  context.fillRect(player.x, player.y, player.width, player.height);
 
-    requestAnimationFrame(update);
-    document.addEventListener("keydown", movePlayer);
-}
+  requestAnimationFrame(update);
+  document.addEventListener("keydown", movePlayer);
+};
 
 function update() {
-    requestAnimationFrame(update);
-    context.clearRect(0, 0, board.width, board.height);
+  requestAnimationFrame(update);
+  context.clearRect(0, 0, board.width, board.height);
 
-    context.fillStyle = "lightgreen";
-    context.fillRect(player.x, player.y, player.width, player.height);
+  context.fillStyle = "lightgreen";
+  context.fillRect(player.x, player.y, player.width, player.height);
 
-    context.fillStyle = "white";
-    ball.x += ball.velocityX;
-    ball.y += ball.velocityY;
-    context.fillRect(ball.x, ball.y, ball.width, ball.height);
+  context.fillStyle = "white";
+  ball.x += ball.velocityX;
+  ball.y += ball.velocityY;
+  context.fillRect(ball.x, ball.y, ball.width, ball.height);
+
+  if(ball.y <=0 ) {
+    ball.velocityY *= -1;
+  }
+  else if(ball.x <= 0 || (ball.x + ball.width) >= boardWidth) {
+    ball.velocityX *= -1;
+  }
+  else if(ball.y + ball.height >= boardHeight) {
+
+  }
+  if(topCollision(ball, player) || bottomCollision(ball, player)) {
+    ball.velocityY *= -1;
+  }
+  else if(leftCollision(ball, player) || rightCollision(ball, player)) {
+    ball.velocityX *= -1;
+  }
 }
 
 function outOfBounds(xPosition) {
-    return(xPosition < 0 || xPosition + playerWidth > boardWidth);
+  return xPosition < 0 || xPosition + playerWidth > boardWidth;
 }
 
 function movePlayer(e) {
+  if (e.code == "ArrowLeft") {
+    let nextPlayerX = player.x - player.velocityX;
+    if (!outOfBounds(nextPlayerX)) {
+      player.x = nextPlayerX;
+    }
+  } else if (e.code == "ArrowRight") {
+    let nextPlayerX = player.x + player.velocityX;
+    if (!outOfBounds(nextPlayerX)) {
+      player.x = nextPlayerX;
+    }
+  }
+}
 
-    if(e.code == "ArrowLeft") {
-        // player.x -=player.velocityX;
-        let nextPlayerX = player.x - player.velocityX;
-        if(!outOfBounds(nextPlayerX)) {
-            player.x = nextPlayerX;
-        }
-    }
-    else if(e.code == "ArrowRight") {
-        // player.x += player.velocityX;
-        let nextPlayerX = player.x + player.velocityX;
-        if(!outOfBounds(nextPlayerX)) {
-            player.x = nextPlayerX;
-        }
-    }
+function detectCollision(a, b) {
+    return a.x < b.x + b.width && 
+            a.x + a.width > b.x &&
+            a.y < b.y + b.height &&
+            a.y + a.height > b.y;
+}
+
+function topCollision(ball, block) {
+    return detectCollision(ball, block) && (ball.y + ball.height) >= block.y;
+}
+
+function bottomCollision(ball, block) {
+    return detectCollision(ball, block) && (block.y + block.height) >= ball.y;
+}
+
+function leftCollision(ball, block) {
+    return detectCollision(ball, block) && (ball.x + ball.width) >= block.x;
+}
+
+function rightCollision(ball, block) {
+    return detectCollision(ball, block) && (block.x + block.width) >= ball.x;
 }
